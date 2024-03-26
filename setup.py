7a3bd3171f3ee22a936ddebb073282c630f42d73
Daniel Pope <mauve@mauveweb.co.uk>
2024-03-26 08:50:25 +0000

    Revert to older setuptools configuration

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from Cython.Build import cythonize
 
 setup(
+    long_description=Path('README.md').read_text(encoding='utf8'),
+    long_description_content_type='text/markdown',
     ext_modules=cythonize("wasabigeom.pyx", compiler_directives={'embedsignature': True}),
     zip_safe=False,
 )
